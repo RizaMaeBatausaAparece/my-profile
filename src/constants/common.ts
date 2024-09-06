cc8420f04857f5b147a8f33acab618a90b44a02b
RizaMaeBatausaAparece <rizamaebatausaaparece@gmail.com>
2024-09-06 08:28:18 +0800

    first commit

---
+++ src/constants/common.ts
@@ -0,0 +1,15 @@
+export const NAME = 'Riza Aparece';
+
+// ABOUT PAGE
+export const ABOUT_TEXT_1 =
+  'Aspiring software developer currently pursuing a degree in BS Information Technology. Skilled in writing, testing, debugging, and maintaining software code to create functional applications and systems. Have foundational knowledge in programming languages, algorithms, data structures, and software development methodologies.';
+export const ABOUT_TEXT_2 =
+  'Focused on learning and developing skills in web applications, both frontend and backend, while collaborating with peers to design and implement scalable solutions.';
+export const ABOUT_TEXT_3 =
+  'Versatile student gaining proficiency in both frontend and backend development, aiming to build complete web applications from start to finish. Continuously learning various programming languages, frameworks, and databases.';
+
+// RESUME PAGE
+export const RESUME_TEXT_1 =
+  '4th-year BS Information Technology student with a strong interest in building websites and web applications. Developing knowledge in modern JavaScript tools/frameworks and exploring both front-end and back-end technologies.';
+export const RESUME_TEXT_2 =
+  '4th-year student currently pursuing a BS Information Technology degree, with growing experience in web development using modern JavaScript tools/frameworks.';
